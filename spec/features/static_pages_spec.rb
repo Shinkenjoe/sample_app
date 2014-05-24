676d36ac8cf15144a87f57611af4c503a0526e1d
shinkenjoe <simomayer@gmail.com>
2014-05-24 20:19:00 +0200

    add contact   and   move to postgres

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -31,5 +31,17 @@ describe "StaticPages" do
       expect(page).to have_selector('h1', text: 'About Us')
     end
   end
+  describe "Contact Page" do
+    it "should have the right title Contact'" do
+      visit '/static_pages/contact'
+      expect(page.title).to eql ('Sample App | Contact')
+    end
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('h1', text: 'Contact')
+    end
+  end
+
+
 
 end
